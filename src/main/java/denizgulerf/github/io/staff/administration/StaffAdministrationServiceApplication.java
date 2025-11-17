eaeae7cb4c8c16245e18892599913f0a1b3e0ada
Deniz Güler <21000057@emu.edu.tr>
2025-11-17 15:10:10 +0300

    initial commit

---
+++ src/main/java/denizgulerf/github/io/staff/administration/StaffAdministrationServiceApplication.java
@@ -0,0 +1,13 @@
+package denizgulerf.github.io.staff.administration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaffAdministrationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StaffAdministrationServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/denizgulerf/github/io/staff/administration/StaffAdministrationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package denizgulerf.github.io.staff.administration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StaffAdministrationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
